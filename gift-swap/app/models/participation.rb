e7a4af3c463ad7ef08d088d3835e5f58741c9961
David Skaggs <davidtskaggs@gmail.com>
2017-11-10 15:51:13 -0500

    Backup push of correct user event participaton models

--- gift-swap/app/models/participation.rb
+++ gift-swap/app/models/participation.rb
@@ -1,6 +1,5 @@
 class Participation < ActiveRecord::Base
   belongs_to :participant, class_name: "User", foreign_key: :participant_id
   belongs_to :event
-  has_one :creator, through: :event
-
+  
 end
