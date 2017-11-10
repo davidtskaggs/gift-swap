f2b5abee18cd1ab96d5ae2cfc7bd5f87a707f374
David Skaggs <davidtskaggs@gmail.com>
2017-11-10 00:46:16 -0500

    Test event creation functionality by adding routes and link in nav bar

--- gift-swap/config/routes.rb
+++ gift-swap/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :events
   devise_for :users
   resources :welcome
+  resources :events
   root to: redirect('/welcome/show')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
