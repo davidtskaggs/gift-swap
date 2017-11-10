e7bfdcf2053d6799bd314bad942376f8ab503947
David Skaggs <davidtskaggs@gmail.com>
2017-11-09 20:32:47 -0500

    Add user seed data

--- gift-swap/db/seeds.rb
+++ gift-swap/db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.delete_all
+
+thierry = User.create(email: "thierry@gmail.com", password: "123456")
+catherine = User.create(email: "catherine@gmail.com", password: "123456")
+david = User.create(email: "david@gmail.com", password: "123456")
+andres = User.create(email: "andres@gmail.com", password: "123456")
+
+30.times do
+  User.create(email: Faker::Internet.email, password: Faker::Internet.password(6, 15))
+end
