5d4b7f4707c6c2f672b3a5e22c1a04a7683a214e
David Skaggs <davidtskaggs@gmail.com>
2017-11-10 09:45:33 -0500

    Revise assoc

--- gift-swap/app/models/user.rb
+++ gift-swap/app/models/user.rb
@@ -5,10 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :events
-  has_many :participations
+  has_many :participants, class_name: "User", foreign_key: :participant_id
 
-  has_many :participants, through: :participations
-  has_one :creator, through: :participants
+  # has_many :participants, through: :participations
+  has_many :participants, through: :events
+
+  has_one :creator, class_name: "User", foreign_key: :creator_id
 
   # has_many :participants, through: :events
   # has_many :creators, through: :participations
--- gift-swap/db/seeds.rb
+++ gift-swap/db/seeds.rb
@@ -1,12 +1,15 @@
 require 'faker'
 
 Event.delete_all
+Admin.delete_all
 User.delete_all
 
 thierry = User.create(email: "thierry@gmail.com", password: "123456")
 catherine = User.create(email: "catherine@gmail.com", password: "123456")
 david = User.create(email: "david@gmail.com", password: "123456")
 andres = User.create(email: "andres@gmail.com", password: "123456")
+
+dave = Admin.create(email: "davidtest@test.com", password: 123456)
 
 30.times do
   User.create(email: Faker::Internet.email, password: Faker::Internet.password(6, 15))
